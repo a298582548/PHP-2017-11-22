f96f3022e1b3926c3f1b3e7ff59d434729f6a043
a298582548 <105021010@live.asia.edu.tw>
2017-11-22 12:03:36 +0800

    2017-11-22

---
+++ PHPIDPass.php
@@ -0,0 +1,12 @@
+
+<?php session_status();
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 2017/11/22
+ * Time: 上午 10:37
+ */
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+$link = mysql_connect("localhost","my_user","my_password","my_email");
